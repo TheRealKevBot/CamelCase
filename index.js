2863a8e9b880430a0b666359302b9660bd381b52
TheRealKevBot <Kglas001@gmail.com>
2021-06-15 18:40:08 -0600

    First

---
+++ index.js
@@ -0,0 +1,11 @@
+// Write simple .camelCase method (camel_case function in PHP, CamelCase in C# or camelCase in Java) for strings. 
+// All words must have their first letter capitalized without spaces.
+
+String.prototype.camelCase=function(){
+    //your code here
+}
+
+console.log("test case".camelCase(), "TestCase");
+console.log("camel case method".camelCase(), "CamelCaseMethod");
+console.log("say hello ".camelCase(), "SayHello");
+console.log(" camel case word".camelCase(), "CamelCaseWord");
